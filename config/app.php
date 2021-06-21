6dbbd49e6a50d942902b3c59790c5dac5f75c8c6
vedprakashn <vedprakash151994@gmail.com>
2021-06-21 11:49:26 +0530

    env setup

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'https://localhost'),
+    'url' => env('APP_URL', 'https://enigmatic-savannah-54968.herokuapp.com/'),
 
     'asset_url' => env('ASSET_URL', null),
 
